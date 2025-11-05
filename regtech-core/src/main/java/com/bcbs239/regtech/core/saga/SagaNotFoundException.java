6c90903853525eda5a4f282f52cf744a9ddca099
Alseny Cisse <a.cisse@basedigitalegroup.com>
2025-11-05 10:45:12 +0100

    refactoring

--- regtech-core/src/main/java/com/bcbs239/regtech/core/saga/SagaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bcbs239.regtech.core.saga;
-
-public class SagaNotFoundException extends RuntimeException {
-    public SagaNotFoundException(SagaId sagaId) {
-        super("Saga not found with id: " + sagaId.id());
-    }
-}
--- regtech-core/src/main/java/com/bcbs239/regtech/core/saga/SagaStatus.java
+++
@@ -1,6 +0,0 @@
-package com.bcbs239.regtech.core.saga;
-
-
-public enum SagaStatus {
-    STARTED, IN_PROGRESS, COMPLETED, FAILED, COMPENSATING
-}
