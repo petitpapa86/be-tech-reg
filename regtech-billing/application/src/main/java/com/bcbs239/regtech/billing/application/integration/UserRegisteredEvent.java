45c5aa53ff99a159e63ce410472bea94c4fba608
Alseny Cisse <a.cisse@basedigitalegroup.com>
2025-11-07 15:21:48 +0100

    Refactor: Introduce UserRegisteredIntegrationEvent and enhance AbstractSaga with retry and error handling mechanisms

---
+++ regtech-billing/application/src/main/java/com/bcbs239/regtech/billing/application/integration/UserRegisteredEvent.java
@@ -0,0 +1,25 @@
+package com.bcbs239.regtech.billing.application.integration;
+
+import com.bcbs239.regtech.core.domain.events.BaseEvent;
+import com.bcbs239.regtech.core.domain.events.IntegrationEvent;
+import lombok.Getter;
+
+@Getter
+public class UserRegisteredIntegrationEvent extends BaseEvent {
+    private final String eventId;
+    private final String aggregateId;
+    private final String correlationId;
+    private final String causationId;
+    private final String userId;
+    private final String email;
+
+    public UserRegisteredIntegrationEvent(String eventId, String aggregateId, String correlationId, String causationId, String userId, String email) {
+        super(correlationId,eventId,aggregateId,causationId);
+        this.eventId = eventId;
+        this.aggregateId = aggregateId;
+        this.correlationId = correlationId;
+        this.causationId = causationId;
+        this.userId = userId;
+        this.email = email;
+    }
+}
